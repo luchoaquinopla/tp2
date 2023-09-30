c7b67df719770578393701caec84b636ff7aea9a
luchoaquinopla <luchoaquinopla@gmail.com>
2023-09-30 20:11:34 -0300

    3 commit 30/9

--- src/main/java/com/correo/ucp/Buzon.java
+++ src/main/java/com/correo/ucp/Buzon.java
@@ -4,8 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Buzon {
-public List<Correo> bandejaEntrada = new ArrayList<>();
- public List<Correo> bandejaEnviados = new ArrayList<>();
+    private String correo;
+    public List<Correo> bandejaEntrada = new ArrayList<>();
+    public List<Correo> bandejaEnviados = new ArrayList<>();
+    public Buzon(){
+
+    }
+    public Buzon(String correo, List<Correo> bandejaEntrada, List<Correo> bandejaEnviados){
+        this.correo = correo;
+        this.bandejaEntrada = bandejaEntrada; 
+        this.bandejaEnviados = bandejaEnviados;
+
+    }
+
  
  public List<Correo> getBandejaEntrada() {
     return bandejaEntrada;
--- src/test/java/BuzonTest.java
+++ src/test/java/BuzonTest.java
@@ -1,14 +1,17 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
+import java.util.ArrayList;
+import com.correo.ucp.Contacto;
 import org.junit.Test;
 
 import com.correo.ucp.Buzon;
+import com.correo.ucp.Correo;
 
 public class BuzonTest {
     @Test
     public void testGetBandejaEntrada() {
-     
-        Buzon buzon = new Buzon();
+        
+        Buzon buzon = new Buzon("asdasfqasd",new ArrayList<>(), new ArrayList<>());
         assertTrue(buzon.getBandejaEntrada().isEmpty());
     }
 
@@ -18,5 +21,30 @@ public class BuzonTest {
         Buzon buzon = new Buzon();
         assertTrue(buzon.getBandejaEnviados().isEmpty());
     }
+    @Test
+    public void testAgregarElementos() {
+        Contacto remitente = new Contacto();
+        // Crear una instancia de Buzon
+        Buzon buzon = new Buzon("correo@example.com", new ArrayList <>(), new ArrayList<>());
+
+        // Crear instancias de Correo
+        Correo correo1 = new Correo("Asunto 1", "Contenido 1", remitente);
+        Correo correo2 = new Correo("Asunto 2", "Contenido 2", remitente);
+
+        // Agregar elementos a la bandeja de entrada
+        buzon.getBandejaEntrada().add(correo1);
+        buzon.getBandejaEntrada().add(correo2);
+
+        // Agregar elementos a la bandeja de enviados
+        buzon.getBandejaEnviados().add(correo1);
+
+        // Verificar si los elementos se han agregado correctamente
+        assertEquals(2, buzon.getBandejaEntrada().size()); // Debería haber 2 elementos en la bandeja de entrada
+        assertEquals(1, buzon.getBandejaEnviados().size()); // Debería haber 1 elemento en la bandeja de enviados
+        assertTrue(buzon.getBandejaEntrada().contains(correo1)); // La bandeja de entrada debe contener correo1
+        assertTrue(buzon.getBandejaEntrada().contains(correo2)); // La bandeja de entrada debe contener correo2
+        assertTrue(buzon.getBandejaEnviados().contains(correo1)); // La bandeja de enviados debe contener correo1
+    }
 }
+
 
